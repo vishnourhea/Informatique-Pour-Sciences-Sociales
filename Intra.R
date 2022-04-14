1461c9d3cb4ff1c50464531b217bbf061fd6ad08
Dave Will JEAN LOUIS <dwjeanlouis@sogebank.com>
2022-04-14 19:12:39 -0400

    Update

--- Intra.R
+++ Intra.R
@@ -8,5 +8,23 @@ library(readxl)
 #Importation du fichier contenant les donnees de la banque mondiale
 data <- read_excel("WB_HT_DATA.xlsx")
 
-#On enleve les trois premieres lignes
-ggplot(data=data, aes(x = date, y = liquidity)) + geom_line()
+#Graphique de figure 2.4
+ggplot(data, aes(date, liquidity, group = 1)) + 
+  geom_line(color = "green", size = 1.5) + 
+  geom_smooth(se = FALSE, color = "red") +
+  labs(x = "Années", y = "%PIB", title = "Figure 2.4 - Ratio des réserves liquides des banques sur leurs actif (%)") +
+  theme(plot.title = element_text(hjust = 0.5),
+        panel.background = element_rect(fill = "transparent",colour = NA),
+        plot.background = element_rect(fill = "transparent",colour = NA)) +
+  geom_vline(xintercept = c(2008, 2010, 2015, 2016, 2020), linetype="dotted", 
+             color = "red", size=1)
+
+#Graphique de la figure 3.1
+ggplot(data, aes(date, transfers, group = 1)) + 
+  geom_line(color = "green", size = 1.5) + 
+  labs(x = "Années", y = "%PIB", title = "Figure 3.1 - Evolution des transferts annuels (en % du PIB)") +
+  theme(plot.title = element_text(hjust = 0.5),
+        panel.background = element_rect(fill = "transparent",colour = NA),
+        plot.background = element_rect(fill = "transparent",colour = NA)) +
+  geom_vline(xintercept = c(2008, 2010, 2016, 2019, 2020), linetype="dotted", 
+             color = "red", size=1)
